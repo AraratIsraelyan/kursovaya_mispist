bb4dd91bb20c7f36b90aeeb985092e54f3532405
Ararat Israelyan <ararat.israelyan@mail.ru>
2020-11-15 01:53:47 +0300

    первичная настройка

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=kursovaya_mispisit',
+    'username' => 'mysql',
+    'password' => 'mysql',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
